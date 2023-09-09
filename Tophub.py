4af237c68980786f9a1f6db6b69df33c9277ab62
fatwang2 <wangding0798@gmail.com>
2023-09-09 11:59:00 +0800

    修改为网易新闻

--- Tophub.py
+++ Tophub.py
@@ -25,18 +25,14 @@ class Tophub(Plugin):
         if content == "热榜":  # 如果消息内容为 "热榜"
             token = conf().get("tophub_token")  # 从配置文件中获取 tophub_token
             news_type = conf().get("tophub_type") # 从配置文件中获取 tophub_type
-            url = "http://v.juhe.cn/toutiao/index"  # API 的 URL
-            params = {"key": token, "type": news_type}
+            url = "https://v2.alapi.cn/api/new/toutiao"  # API 的 URL
+            payload = f"token={token}&type={news_type}&page=1"
             headers ={"Content-Type": "application/x-www-form-urlencoded"}# 请求头
 
             try:
-                resp = requests.get(url,params=params,headers=headers)# 发送 get 请求
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred when making the request: {e}")  # 请求出错时打印错误消息
-                return
-
-            data = json.loads(resp.text)  # 解析返回的 JSON 数据
-            news_data = data.get('result').get('data')  # 获取新闻数据
+                resp = requests.request("POST", url, data=payload, headers=headers)# 发送 get 请求
+                data = json.loads(resp.text)  # 解析返回的 JSON 数据
+                news_data = data.get('data')  # 获取新闻数据
             if news_data:
                 reply = Reply()  # 创建回复消息对象
                 reply.type = ReplyType.TEXT  # 设置回复消息的类型为文本
@@ -44,13 +40,11 @@ class Tophub(Plugin):
 
                 for i, news_item in enumerate(news_data, 1):
                     title = news_item.get('title', '未知标题') # 获取新闻标题
-                    link = news_item.get('url', '未知链接') # 获取新闻链接
-                    date = news_item.get('date', '未知日期') # 获取新闻时间
-                    # 删除任何前置的数字和特殊字符（如果有）
-                    title = re.sub(r'^\d+[、.]\s*', '', title)
+                    link = news_item.get('m_url', '未知链接') # 获取新闻链接
+                    digest = news_item.get('digest', '未知摘要') # 获取新闻摘要
 
                     # 添加到回复内容中
-                    reply.content += f"{i}. {title}\n{date}\n{link}\n\n"
+                    reply.content += f"{i}. {title}\n{digest}\n{link}\n\n"
 
                 e_context["reply"] = reply
                 e_context.action = EventAction.BREAK_PASS
